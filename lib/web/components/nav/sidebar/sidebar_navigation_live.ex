1a92517f27a6caade54b18ce14551024b59f69b3
Mayel <git@mayel.space>
2021-07-03 15:35:07 +0200

    add sidebar

---
+++ lib/web/components/nav/sidebar/sidebar_navigation_live.ex
@@ -0,0 +1,7 @@
+defmodule Bonfire.Breadpub.SidebarNavigationLive do
+  use Bonfire.Web, :stateless_component
+  alias Surface.Components.LivePatch
+
+  prop page, :string, required: true
+  prop username, :string, required: true
+end
