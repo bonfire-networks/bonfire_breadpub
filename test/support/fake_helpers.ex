09a89f670320fc9e3b060cd5edac1408e6185f2a
Mayel de Borniol <git@mayel.space>
2022-03-10 18:29:11 +1300

    clean

--- test/support/fake_helpers.ex
+++ test/support/fake_helpers.ex
@@ -7,15 +7,6 @@ defmodule Bonfire.Breadpub.Test.FakeHelpers do
 
   import Bonfire.Breadpub.Integration
 
-  def fake_account!(attrs \\ %{}) do
-    cs = Accounts.signup_changeset(Fake.account(attrs))
-    assert {:ok, account} = repo().insert(cs)
-    account
-  end
 
-  def fake_user!(%Account{}=account, attrs \\ %{}) do
-    assert {:ok, user} = Users.create(Fake.user(attrs), account)
-    user
-  end
 
 end
