f3e51ee428665a64ba331bd6ab2ad367cdf39e38
Mayel de Borniol <git@mayel.space>
2022-04-22 08:11:44 +1200

    c

--- mix.exs
+++ mix.exs
@@ -23,12 +23,7 @@ defmodule Bonfire.Breadpub.MixProject do
     ]
   end
 
-  def application do
-    [
-      # mod: {Bonfire.Breadpub.FallbackApplication, []},
-      extra_applications: [:logger, :runtime_tools]
-    ]
-  end
+  def application, do: [ extra_applications: [:logger, :runtime_tools] ]
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
