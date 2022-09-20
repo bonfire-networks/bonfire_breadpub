3b044805f9f57199f5662b2f1fb2774816bdce63
Mayel de Borniol <git@mayel.space>
2022-09-20 16:59:13 +1200

    https://github.com/bonfire-networks/bonfire-app/issues/445

--- lib/web/components/create_intent/create_intent_live.ex
+++ lib/web/components/create_intent/create_intent_live.ex
@@ -3,6 +3,7 @@ defmodule Bonfire.UI.Breadpub.CreateIntentLive do
 
   prop intent_url, :string, required: false, default: ""
   prop action_id, :string, required: false, default: "work"
+  prop in_scope_of, :string, required: false
   prop output_of_id, :string, required: false
   prop title, :string, default: "Create a new intent"
   prop intent_type, :string, default: "need"
