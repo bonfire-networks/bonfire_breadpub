54be7b2d4d93da96d7c8464f352b0456a95dee60
Mayel de Borniol <git@mayel.space>
2025-05-03 20:07:48 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1239

--- lib/integration.ex
+++ lib/integration.ex
@@ -1,6 +1,8 @@
 defmodule Bonfire.Breadpub do
   @moduledoc "./README.md" |> File.stream!() |> Enum.drop(1) |> Enum.join()
 
+  use Bonfire.Common.Config
+
   def repo, do: Bonfire.Common.Config.repo()
 
   def mailer, do: Bonfire.Common.Config.get!(:mailer_module)
